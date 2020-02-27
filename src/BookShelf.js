ea5545ae7414142f134ea5c98b487b80b305027e
khelalfa rabeh <khelalfarabeh1996@gmail.com>
2020-02-27 22:39:37 +0100

    Filter the books for each shelf.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -3,14 +3,21 @@ import React from 'react'
 import Book from './Book';
 
 const BookShelf = (props) => {
+
+	// Destructuring  props' values
 	const  {books, shelf} = props;
+
+	// get the only books that match the shelf
+	const booksUponShelf = books.filter((book) => book.shelf === shelf);
+
 	return(
 		<div className="bookshelf">
 			<h2 className="bookshelf-title">{shelf}</h2>
 			<div className="bookshelf-books">
 				<ol className="books-grid">
 
-				{books.map((book) => (
+			{/* Map through books to create a Book Component for each book */}
+				{booksUponShelf.map((book) => (
 					<li>
 						<Book book={book}/>
 					</li>
