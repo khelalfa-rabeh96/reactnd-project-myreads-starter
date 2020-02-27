72c8a803eedb3b547421ad23b8e2bda0ae8bdfc5
khelalfa rabeh <khelalfarabeh1996@gmail.com>
2020-02-28 00:27:08 +0100

    Create a ShelfChanger Componet that will handle select a shelf option

--- src/Book.js
+++ src/Book.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-
+import ShelfChanger from './ShelfChanger';
 
 const Book = (props) => {
 
   // Destructuring  props' values
   const {book} = props;
 
+ 
+
+  const changeCurrentShelf = (shelf) => {
+    props.onChangeBookShelf(book.id, shelf)
+  }
+
   return(
       <div className="book">
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.cover_url})` }}></div>
-          <div className="book-shelf-changer">
-            <select>
-              <option value="move" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+          <ShelfChanger onChangeCurrentShelf={changeCurrentShelf}/>
         </div>
 
         <div className="book-title">{book.title}</div>
@@ -31,6 +29,7 @@ const Book = (props) => {
 
 Book.propTypes={
   book: PropTypes.object.isRequired,
+  onChangeBookShelf: PropTypes.func.isRequired,
 }
 
 export default Book;
---
+++ src/ShelfChanger.js
@@ -0,0 +1,36 @@
+import React, { Component }  from 'react';
+import PropTypes from 'prop-types'
+
+class ShelfChanger extends Component {
+
+	static propTypes = {
+		onChangeCurrentShelf: PropTypes.func.isRequired,
+	}
+
+	state = {
+		selected:"move",
+	}
+
+	// Handle the select option and 
+	// Call the parent call back to change the book shelf
+	hendleSelectShelf = (event) => {
+	    const shelf = event.target.value;
+	    this.props.onChangeCurrentShelf(shelf);
+	}
+
+	render(){
+		return (
+			<div className="book-shelf-changer">
+	            <select onChange={this.hendleSelectShelf} value={this.state.selected} >
+	              <option value="move" disabled>Move to...</option>
+	              <option value="currentlyReading">Currently Reading</option>
+	              <option value="wantToRead">Want to Read</option>
+	              <option value="read">Read</option>
+	              <option value="none">None</option>
+	            </select>
+			</div>
+			)
+	}
+}
+
+export default ShelfChanger;
