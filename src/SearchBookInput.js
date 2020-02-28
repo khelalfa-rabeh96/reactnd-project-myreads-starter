2f51e5554e8bc580b8342c59914e61ffc7645fe1
khelalfa rabeh <khelalfarabeh1996@gmail.com>
2020-02-28 11:58:19 +0100

    Feat: Get the search term from book search input

--- src/SearchBookInput.js
+++ src/SearchBookInput.js
@@ -2,6 +2,24 @@ import React, { Component }  from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBookInput extends Component {
+
+	static propTypes = {
+		onHandleSearch: PropTypes.func.isRequired,
+	}
+
+	state = {
+		query: '',
+	}
+
+	handleChange = (event) => {
+		
+		// Get the search term and then send it  back
+		// To the search page component with the onHandleSearch callback
+		// To let the search page handle the results of this term
+		const query = event.target.value;
+		this.setState(() => ({query:query}));
+		this.props.onHandleSearch(event.target.value);
+	}
 	render(){
 		return(
 			<div className="search-books-input-wrapper">
@@ -13,7 +31,11 @@ class SearchBookInput extends Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
+                <input 
+                	type="text" 
+					placeholder="Search by title or author"
+					valeu={this.state.query}
+					onChange={this.handleChange}/>
 
               </div>
 		)
