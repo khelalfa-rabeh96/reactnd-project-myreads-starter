e01d89852ea5d47b33c9ea05789f9588d43d0718
khelalfa rabeh <khelalfarabeh1996@gmail.com>
2020-02-29 11:12:50 +0100

    refactor: Replace the static data with data from the api request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import './App.css'
+import * as BooksAPI from './BooksAPI'
 
 import BookShelf from './BookShelf';
 import SearchPage from './SearchPage';
+
 
 class BooksApp extends React.Component {
 
@@ -14,33 +16,20 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     showSearchPage: false,
-    books: [
-      { 
-        id:1,
-        title:"To Kill a Mockingbird",
-        author: "Harper Lee",
-        cover_url:"http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api", 
-        shelf: "currentlyReading"
-      },
-      { 
-        id:2,
-        title:"Ender's Game",
-        author: "Orson Scott Card",
-        cover_url:"http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api", 
-        shelf: "wantToRead",
-      },
-
-      { 
-        id:3,
-        title:"1776",
-        author: "David McCullough",
-        cover_url: "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",  
-        shelf: "read",
-      },
-    ]
+    books: []
   }
-
-
+  
+  componentDidMount =() => {
+    console.log(BooksAPI.getAll())
+    BooksAPI.getAll().then(books => {
+       this.setState(() => ({
+          books: books
+        }))
+       console.log(books)
+      })
+    
+  }
+  
   showSearchPage = () => {
     this.setState(() =>({
       showSearchPage:false
@@ -71,6 +60,7 @@ class BooksApp extends React.Component {
   }
 
   render() {
+    console.log(this.state.books);
     return (
       <div className="app">
         {this.state.showSearchPage ? (
